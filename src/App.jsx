2a3fc885ecff2726befdc046012430c3bc7b7b59
Shahzaib-Maqbool <shahzaib405022@gmail.com>
2026-08-01 03:44:40 +0500

    build sucess

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
 import Layout from './components/layout/Layout'
 import AppRoutes from './routes/AppRoutes'
 import FloatingChat from './components/ui/FloatingIcon'
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }, [])
+
   return (
     <Layout>
       <AppRoutes />
